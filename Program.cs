5f230b828a78d0123fc51c51f078e3c910c73353
F. X. Kevin Koesnadi <58078219+FXKevinK@users.noreply.github.com>
2021-03-24 08:15:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+            Console.WriteLine("Saya, Fransiskus Xaverius Kevin Koesnadi");
+            Console.WriteLine("05111840000162");
+            Console.WriteLine("Kelas, PBKK-C\n");
+            Console.WriteLine("Aplikasi yang pernah saya kembangkan:");
+            Console.WriteLine("- E-PSU Kabupaten Sidoarjo (Django)");
+            Console.WriteLine("- Web Profile (Native)");
+            Console.WriteLine("- Simple Library (Laravel)");
+            Console.WriteLine("\nDemikian, terima kasih!");
+        }
+    }
+}
